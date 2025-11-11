63ee97c5ae3fb0f776639cd468dcc118fe330e6d
cian74 <cianbarrett3011@gmail.com>
2025-11-11 14:22:44 +0000

    fix: changed path to jar to be relative so multiple platforms can access jar

--- lua/refactor/backend.lua
+++ lua/refactor/backend.lua
@@ -1,41 +1,43 @@
 local Job = require('plenary.job')
 local M = {}
+local cwd = vim.fn.getcwd()
+local jar_path = cwd .. "/backend/target/java-refactor-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 function M.start_backend()
-  M.job = Job:new({
-    command = "java",
-    args = {"-jar", "/home/cian/Work/java-refactor-tool/backend/target/java-refactor-1.0-SNAPSHOT-jar-with-dependencies.jar"},
+	M.job = Job:new({
+		command = "java",
+		args = {"-jar", jar_path, },
 
-    on_stdout = function(_, data)
-      if data and data ~= "" then
-        vim.schedule(function()
-          print("[Java Backend]", data)
-        end)
-      end
-    end,
+	on_stdout = function(_, data)
+		if data and data ~= "" then
+			vim.schedule(function()
+				print("[Java Backend]", data)
+			end)
+		end
+	end,
 
-    on_stderr = function(_, err)
-      if err and err ~= "" then
-        vim.schedule(function()
-          vim.notify("Backend error: " .. err, vim.log.levels.ERROR)
-        end)
-      end
-    end,
-  })
+	on_stderr = function(_, err)
+		if err and err ~= "" then
+			vim.schedule(function()
+				vim.notify("Backend error: " .. err, vim.log.levels.ERROR)
+			end)
+		end
+	end,
+})
 
-  M.job:start()
+M.job:start()
 end
 
 function M.send_request(request)
-  if not M.job then
-    vim.schedule(function()
-      vim.notify("Backend not running", vim.log.levels.ERROR)
-    end)
-    return
-  end
+	if not M.job then
+		vim.schedule(function()
+			vim.notify("Backend not running", vim.log.levels.ERROR)
+		end)
+		return
+	end
 
-  local json = vim.fn.json_encode(request)
-  M.job:send(json .. "\n")
+	local json = vim.fn.json_encode(request)
+	M.job:send(json .. "\n")
 end
 
 return M
